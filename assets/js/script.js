868eca2a23ee7d8b6be96a1feeff13a2b5ca443c
amanayak <amannayak2911@gmail.com>
2025-03-23 18:44:56 +0530

    add new product's / AMAN

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function showCard() {
-    const card = document.getElementById('canterMePage');
-    card.style.display = "flex" // Adds the 'active' class    
-    const cardimg = document.getElementById('cardimg');
-    cardimg.classList.add('active'); // Adds the 'active' class
-}
